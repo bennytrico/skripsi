c988fbb8d4530d61fd5618e8625765522a323160
bennytrico <benny.liumanto@gmail.com>
2019-04-13 15:47:13 +0700

    firebase auth

--- app/src/main/java/com/example/skripsicustomer1/RegisterPage.java
+++ app/src/main/java/com/example/skripsicustomer1/RegisterPage.java
@@ -4,12 +4,20 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.WindowManager;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class RegisterPage extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_page);
         this.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
+
+        mAuth = FirebaseAuth.getInstance();
+
     }
 }
