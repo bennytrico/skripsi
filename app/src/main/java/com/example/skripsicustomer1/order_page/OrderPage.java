354e562ad37c8c4fc48f8b4bdd0b5cf5b0dd628b
bennytrico <benny.liumanto@gmail.com>
2019-05-01 13:26:49 +0700

    updated

--- app/src/main/java/com/example/skripsicustomer1/order_page/OrderPage.java
+++ app/src/main/java/com/example/skripsicustomer1/order_page/OrderPage.java
@@ -82,7 +82,7 @@ public class OrderPage extends AppCompatActivity {
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
+    public boolean ongOptionsItemSelected(MenuItem item) {
         Intent myIntent = new Intent(getApplicationContext(), HomePage.class);
         startActivityForResult(myIntent, 0);
         return true;
