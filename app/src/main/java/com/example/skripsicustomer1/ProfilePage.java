2ff944b2ea34d04cfadbf8cf4045b7c9323dd1f4
bennytrico <benny.liumanto@gmail.com>
2019-06-23 19:29:45 +0700

    get user data on profile page

--- app/src/main/java/com/example/skripsicustomer1/ProfilePage.java
+++ app/src/main/java/com/example/skripsicustomer1/ProfilePage.java
@@ -8,20 +8,27 @@ import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.HashMap;
 import java.util.Map;
 
 public class ProfilePage extends AppCompatActivity {
 
+    TextView nameProfile;
+    TextView emailProfile;
+    TextView handPhoneProfile;
     EditText oldPassword;
     EditText newPassword;
     EditText reNewPassword;
@@ -32,12 +39,14 @@ public class ProfilePage extends AppCompatActivity {
     private String formReNewPassword;
     private Boolean flag = false;
 
+    Customer customer = new Customer();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_page);
         getSupportActionBar().setHomeButtonEnabled(true);
         getSupportActionBar().setTitle("Profile");
+        getUserData();
         initVariable();
         submitChangePassword.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -96,6 +105,7 @@ public class ProfilePage extends AppCompatActivity {
                                 Map<String, Object> update = new HashMap<String, Object>();
                                 update.put("password",formNewPassword);
                                 dbCustomer.updateChildren(update);
+                                finish();
                             } else {
                                 Toast.makeText(ProfilePage.this, "Gagal ganti password", Toast.LENGTH_SHORT).show();
                             }
@@ -107,6 +117,26 @@ public class ProfilePage extends AppCompatActivity {
         oldPassword = (EditText) findViewById(R.id.oldPassword);
         newPassword = (EditText) findViewById(R.id.newPassword);
         reNewPassword = (EditText) findViewById(R.id.reNewPassword);
+        nameProfile = (TextView) findViewById(R.id.nameProfile);
+        emailProfile = (TextView) findViewById(R.id.emailProfile);
+        handPhoneProfile = (TextView) findViewById(R.id.handPhoneProfile);
         submitChangePassword = (Button) findViewById(R.id.submitChangePassword);
+    }
+    public void getUserData () {
+        DatabaseReference dbCustomer = FirebaseDatabase.getInstance().getReference("Customers").child(FirebaseAuth.getInstance().getCurrentUser().getUid());
+        dbCustomer.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                customer = dataSnapshot.getValue(Customer.class);
+                nameProfile.setText(customer.getUsername());
+                emailProfile.setText(customer.getEmail());
+                handPhoneProfile.setText(customer.getNumber_handphone());
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
     }
 }
